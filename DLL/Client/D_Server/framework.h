c45f727498a80ab3bb5e81e8f768d625c932a838
oldnumber18 <117345554+oldnumber18@users.noreply.github.com>
2024-09-21 11:15:20 +0800

    Add files via upload

--- DLL/Client/D_Server/framework.h
+++ DLL/Client/D_Server/framework.h
@@ -3,6 +3,7 @@
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
 #pragma warning(disable:4996)
 #pragma warning(disable:4101)
+#pragma warning(disable:4200)
 // Windows 头文件
 #include <windows.h>
 #include <winSock2.h>
@@ -10,9 +11,14 @@
 #include <stdlib.h>
 #include <vector>
 using namespace std;
-#define DATA_BUFMAX 2048
-#define DATA_BUFMIN 45
-#define DATA_BUFMID 2000
+#define GetServerInfo_MAX_SumSize 4132
+#define GetServerInfo_Other_SumSize 36
+#define SendServerInfo_Other_SumSize 26
+#define Note_MAX_Size 4130
+#define DATA_BUFMAX 4096
+#define DATA_BUFMID 2048
+#define DATA_BUFMIN 1024
+#define DATA_MIN 512
 #define IP_PORT 9999
 #define IP_ADDR "127.0.0.1"
 #pragma comment(lib,"ws2_32.lib")
@@ -23,17 +29,31 @@ extern "C" {
 typedef struct _WHO {
     int Num;
     char Name[20];
-}IDINFO;
-typedef struct _SERVER {
-    IDINFO ID;// 接收:谁发生过来的信息 发送:发送给谁？-->信息反馈目标
-    int Size;// 消息大小，在 Command 的消息为 FileInfo 时，为文件大小
+}who;
+typedef struct _GetSystemInfo {// 解析包
+    who ID;// 接收:谁发生过来的信息 发送:发送给谁？-->信息反馈目标
     char Type[2];// 发送的类型 第一次区分指令
     char Command[10];// 第二次区分指令
-    char Info[DATA_BUFMID];// 要发送的内容
-}SERVER;
-typedef struct _FILE {
+    char Info[];// 要发送的内容
+}GetServerInfo;
+typedef struct _SendSystemInfo {// 发送包
+    who ID;
+    char Type[2];
+    char Command[10];// 第二次区分指令
+    char *Info;
+}SendServerInfo;
+// FILE
+typedef struct _FILE {// 文件信息
+    signed long int Size;
+    char Name[];
+}INFOFILE;
+typedef struct _SAVEFILE {// 保存文件结构
     FILE* fp;
-    int FileSize;// 总大小
-    int GetSize;// 得到的大小
-    char name[200];
-}SAVE_FILE;
+    signed long SizeSUM;
+    signed long ClientSUMSize;
+    char Name[];
+}SAVEFILE;
+typedef struct _CLIENTFILE {// 接收/发送 文件
+    signed int Size;
+    char Note[];
+}CLIENTFILE;
