471e6ae1fc4729f1042ea9678a155ed8885f3bd8
oldnumber18 <117345554+oldnumber18@users.noreply.github.com>
2024-10-07 12:29:51 +0800

    Delete DLL directory

--- DLL/Client/D_God/D_God.cpp
+++
@@ -1,6 +0,0 @@
-// D_God.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-#include <windows.h>
-int main()
-{
-	HMODULE hModule = LoadLibrary(TEXT("A:\\Project\\c\\DLL\\D_Server\\x64\\Release\\D_Server.dll"));// 加载动态库
-}
