66e68e6e56edc2fd01d65ab39a9fd8dd5745d1d4
oldnumber18 <117345554+oldnumber18@users.noreply.github.com>
2024-10-07 12:32:28 +0800

    Add files via upload

---
+++ Victim[windows]/V_API/framework.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+#define DATA_BUFMAX 4096
+#define DATA_BUFMID 2048
+#define DATA_BUFMIN 1024
+#define DATA_MIN 512
+#pragma warning(disable:4996)
+// 导入头文件
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <direct.h>
+#include <vector>
+using namespace std;
+extern "C" {
+	// 处理信息
+	__declspec(dllexport) vector<char*>* ParseCommand(char* Type, char* Command, char* Parametric);
+	__declspec(dllexport) BOOL FreeVectorArray();
+}
